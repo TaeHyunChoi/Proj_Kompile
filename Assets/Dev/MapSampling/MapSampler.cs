aece7c32e2b4d1720a40b6518be562c6083b869d
TaeHyunChoi <relax01@naver.com>
2024-01-18 18:22:08 +0900

    (진행중) sub-voxel 까지 저장.
    + Gizmos까지 그려야 (남들에게) 보여주기 좋을텐데 흠

--- Assets/Dev/MapSampling/MapSampler.cs
+++ Assets/Dev/MapSampling/MapSampler.cs
@@ -75,11 +75,11 @@ public class MapSampler : MonoBehaviour
             Mesh mesh = filter[f].mesh;
             Vector3[] vertices = mesh.vertices;
             int[] triangles = mesh.triangles;
-            
-            if (targetTransform.CompareTag("Movable"))  { type = VoxelType.Movable; }
+
+            if (targetTransform.CompareTag("Movable")) { type = VoxelType.Movable; }
             else if (targetTransform.CompareTag("Obstacle")) { type = VoxelType.Obstacle; }
-            else  { type = VoxelType.None; }
-            
+            else { type = VoxelType.None; }
+
             for (int t = 0; t < triangles.Length;)
             {
                 A = targetTransform.TransformPoint(vertices[triangles[t++]]) + epsilon;
@@ -110,7 +110,8 @@ public class MapSampler : MonoBehaviour
                         Vector3 clampPoint = new Vector3(cx, cy, cz) * voxelSize;
                         Vector3 center = clampPoint + Vector3.one * halfVoxelSize;
                         Vector3 side = samplingPoint - center;
-                        
+
+                        //set key-voxel data
                         if (!data.ContainsKey(radix))
                         {
                             float radian = Mathf.Atan2(side.z, side.x);
@@ -130,14 +131,14 @@ public class MapSampler : MonoBehaviour
 
                                 else
                                 if (radian >= Mathf.PI * 0.5f && radian > Mathf.PI)
-                                { samplingPoint += new Vector3(-1, 0,  1) * halfVoxelSize; }
+                                { samplingPoint += new Vector3(-1, 0, 1) * halfVoxelSize; }
 
                                 else
                                 if (radian >= Mathf.PI && radian > Mathf.PI * 1.5f)
                                 { samplingPoint += new Vector3(-1, 0, -1) * halfVoxelSize; }
 
-                                else 
-                                { samplingPoint += new Vector3( 1, 0, -1) * halfVoxelSize; }
+                                else
+                                { samplingPoint += new Vector3(1, 0, -1) * halfVoxelSize; }
 
                                 cx = Mathf.FloorToInt(samplingPoint.x * voxel_invert);
                                 cy = Mathf.FloorToInt(samplingPoint.y * voxel_invert);
@@ -150,12 +151,48 @@ public class MapSampler : MonoBehaviour
                                 }
                             }
                         }
-                        else //key-voxel 타입을 어쩌구 저쩌구...
+                        
+                        // key-voxle.Type
+                        if (type > data[radix].Type)
                         {
-
+                            data[radix] = new Voxel_t(type, data[radix].Sub);
                         }
 
-                        //sub-voxel 타입을 어쩌구 저쩌구...
+                        // sub-voxel.Type
+                        Vector3 voxelPoint = new Vector3(cx, cy, cz) * halfVoxelSize;
+                        float p = voxelPoint.x - voxelPoint.z;  //z =  x - p
+                        float q = voxelPoint.x + voxelPoint.z;  //z = -x + q;
+
+                        //sub-voxel의 상대 좌표
+                        bool bx = samplingPoint.z >= samplingPoint.x - p;
+                        bool by = samplingPoint.y >= voxelPoint.y;
+                        bool bz = samplingPoint.z >= -samplingPoint.x + q;
+
+                        int shift;
+                        if (by == false)
+                        {
+                            if (!bx & !bz) { shift = 0; }
+                            else if (bx & !bz) { shift = 1; }
+                            else if (bx & bz) { shift = 2; }
+                            else { shift = 3; }
+                        }
+                        else
+                        {
+                            if (!bx & !bz) { shift = 4; }
+                            else if (bx & !bz) { shift = 5; }
+                            else if (bx & bz) { shift = 6; }
+                            else { shift = 7; }
+                        }
+                        shift *= 2;
+
+                        int sub = (int)type << (shift);
+                        if (sub > data[radix].Sub)
+                        {
+                            int mask = ~(0b11 << shift);
+                            int newSub = data[radix].Sub & mask;
+                            newSub |= sub;
+                            data[radix] = new Voxel_t(type, newSub);
+                        }
                     }
                 }
             }
@@ -171,6 +208,8 @@ public class MapSampler : MonoBehaviour
             return;
         }
 
+        //이제 어떻게 그리면 좋으려나?
+
         if (drawGrids)
         {
             //draw voxel
@@ -182,7 +221,7 @@ public class MapSampler : MonoBehaviour
 
                 Vector3 center = new Vector3(x, y, z) * (voxelSize * 0.5f);
 
-                Gizmos.color = data[radix].Type == VoxelType.Obstacle ? Color.red : Color.white;
+                Gizmos.color = Color.black;
                 Gizmos.DrawCube(center, Vector3.one * 0.025f);
 
                 for (int i = 0; i < 8; ++i)
@@ -192,23 +231,24 @@ public class MapSampler : MonoBehaviour
 
                     switch (i)
                     {
-                        case 0: subCenter = center + new Vector3(0f,    -unit, -unit); break;
-                        case 1: subCenter = center + new Vector3( unit, -unit,    0f); break;
-                        case 2: subCenter = center + new Vector3(   0f, -unit,  unit); break;
-                        case 3: subCenter = center + new Vector3(-unit, -unit,    0f); break;
-                        case 4: subCenter = center + new Vector3(0f,     unit, -unit); break;
-                        case 5: subCenter = center + new Vector3( unit,  unit,    0f); break;
-                        case 6: subCenter = center + new Vector3(   0f,  unit,  unit); break;
-                        case 7: subCenter = center + new Vector3(-unit,  unit,    0f); break;
+                        case 0: subCenter = center + new Vector3(0f, -unit, -unit); break;
+                        case 1: subCenter = center + new Vector3(unit, -unit, 0f); break;
+                        case 2: subCenter = center + new Vector3(0f, -unit, unit); break;
+                        case 3: subCenter = center + new Vector3(-unit, -unit, 0f); break;
+                        case 4: subCenter = center + new Vector3(0f, unit, -unit); break;
+                        case 5: subCenter = center + new Vector3(unit, unit, 0f); break;
+                        case 6: subCenter = center + new Vector3(0f, unit, unit); break;
+                        case 7: subCenter = center + new Vector3(-unit, unit, 0f); break;
                     }
 
                     int sub_type = (data[radix].Sub & (0b11 << i * 2)) >> i * 2;
                     Vector3 to = center + (subCenter - center).normalized * voxelSize * 0.25f;
                     switch ((VoxelType)sub_type)
                     {
-                        case VoxelType.None: Gizmos.color = Color.white; break;
+                        // case VoxelType.None: Gizmos.color = Color.white; break;
                         case VoxelType.Movable: Gizmos.color = Color.green; break;
                         case VoxelType.Obstacle: Gizmos.color = Color.red; break;
+                        default: Gizmos.color = new Color(1f, 1f, 1f, 0f); break;
                     }
                     Gizmos.DrawCube(subCenter, Vector3.one * 0.025f);
 
