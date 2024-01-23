72de44575de9e76cec6d27016c324f62d9009308
TaeHyunChoi <relax01@naver.com>
2024-01-23 19:00:26 +0900

    좌우반전 어쩌지;;

--- Assets/Dev/Script/05_Unit/Unit.cs
+++ Assets/Dev/Script/05_Unit/Unit.cs
@@ -9,16 +9,9 @@ public class Unit : MonoBehaviour
 {
     private Vector3 lastDir;
 
-    private void Awake()
-    {
-        lastDir = new Vector3(1f, 0, 1f);
-    }
-
     //Player로 떼어내야지 이거...
-
     public void Move(Dictionary<int, Voxel_t> voxel, Vector3 inputDir)
     {
-
         Vector3 next = transform.position + (inputDir.normalized * MOVE_SPEED * Time.deltaTime);
         Vector3 center = Parser.GetCenterPoint(next);
         int radix = Parser.GetVoxelRadix(center);
@@ -42,30 +35,47 @@ public class Unit : MonoBehaviour
         }
 
         //// neighbor voxels
-        int start = GetSearchStartingIndex(inputDir);
-
-        //이거 rotate가 생각보다 어렵구먼? 3차원.. 저주하리...
-        //이참에 생각 좀 해보자.
+        int start = GetCurrentDirection(inputDir);
         int rotate;
-        if (inputDir.z > 0) { rotate = 1; }
-        else if (inputDir.z < 0) { rotate = -1; }
-        else
+
+        if (inputDir.x != 0 && inputDir.z == 0)
         {
             if (lastDir.z > 0) { rotate = 1; }
             else { rotate = -1; }
         }
-
-        if (lastDir.x > 0)
+        else if (inputDir.x == 0 & inputDir.z != 0)
         {
-            start += 2 * rotate;
-            start %= 8;
+            if (lastDir.x > 0) { rotate = 1; }
+            else { rotate = -1; }
+        }
+        //inputDir.x 와 .z 모두 0이면 입력이 없으므로 ㄴㄴ
+        //최초라면 그대로 입력값을 사용
+        else
+        {
+            if (inputDir.x > 0 || inputDir.z > 0)
+            {
+                rotate = 1;
+            }
+            else
+            {
+                rotate = -1;
+            }
+        }
+
+        if (inputDir.z < 0)
+        {
+            rotate *= -1;
+        }
+        if (rotate == 1)
+        {
+            start = (start + 2) % 8;
         }
         else
         {
-            start -= 2 * rotate;
+            start -= 2;
             if (start < 0)
             {
-                start = 8 + start;
+                start += 8;
             }
         }
 
@@ -107,46 +117,35 @@ public class Unit : MonoBehaviour
                 return;
             }
 
-            //좌우 증감 체크
-            if (lastDir.x > 0)
+            index -= rotate;
+            if (index < 0)
             {
-                index = (index - 1 * rotate) % 8;
-                if (index < 0)
-                {
-                    index += 8;
-                }
-                if (index == start)
-                {
-                    index = (index - 1 * rotate) % 8;
-                }
+                index = 8 + index;
             }
             else
             {
-                index = (index + 1 * rotate) % 8;
-                if (index == start)
-                {
-                    index = (index + 1 * rotate) % 8;
-                }
+                index %= 8;
             }
         }
     }
-    private int GetSearchStartingIndex(Vector3 dir)
+    private int GetCurrentDirection(Vector3 dir)
     {
         // not normalized: x 또는 z 값이 -1, 0, 1 중에 하나로다.
         float x = dir.x;
         float z = dir.z;
 
         //8방향 경우의 수
-        if (x == 0 && z > 0)    { return 0; }
-        if (x > 0  && z > 0)    { return 1; }
-        if (x > 0  && z == 0)   { return 2; }
-        if (x > 0  && z < 0)    { return 3; }
-        if (x == 0 && z < 0)    { return 4; }
-        if (x < 0  && z < 0)    { return 5; }
-        if (x == 0 && z < 0)    { return 6; }
-        if (x < 0  && z > 0)    { return 7; }
+        int current = -1;
+        if (x == 0 && z > 0)    { current = 0; }
+        else if (x > 0  && z > 0)    { current = 1; }
+        else if (x > 0  && z == 0)   { current = 2; }
+        else if (x > 0  && z < 0)    { current = 3; }
+        else if (x == 0 && z < 0)    { current = 4; }
+        else if (x < 0  && z < 0)    { current = 5; }
+        else if (x < 0 && z == 0)    { current = 6; }
+        else if (x < 0  && z > 0)    { current = 7; }
 
-        Debug.Assert(false, "Can`t Find Direction;");
-        return -1;
+        Debug.Assert(current != -1, "Can`t Find Direction;");
+        return current;
     }
 }
