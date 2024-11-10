55cf4d7b19b393dc6248297273b0fbaae27f9520
TaeHyunChoi <relax01@naver.com>
2024-11-10 22:52:10 +0900

    [KOMP-10] pivot에 맞는 mesh 제작
    Slope, SlopHalf, SlopePartial 구현

--- Assets/Script/99_DevEdit/MeshGenerator.cs
+++ Assets/Script/99_DevEdit/MeshGenerator.cs
@@ -5,12 +5,21 @@ public class MeshGenerator : MonoBehaviour
 {
     private static void SaveMeshAsset(string assetName, Mesh mesh)
     {
-        AssetDatabase.CreateAsset(mesh, "Assets/Rcs/MapNav/Mesh/" + assetName + ".asset");
+        var path = "Assets/Rcs/MapNav/Mesh/" + assetName + ".asset";
+        
+        if (AssetDatabase.LoadAssetAtPath<Mesh>(path) != null)
+        {
+            AssetDatabase.DeleteAsset(path);
+        }
+
+        AssetDatabase.CreateAsset(mesh, path);
         AssetDatabase.SaveAssets();
 
         Debug.Log("Mesh asset created: " + assetName);
     }
 
+    //// Cube ////
+    
     [MenuItem("Tools/NavTileMesh/Create/NavMesh_Cube_0")]
     public static void GenerateNavTileMesh_CubeDefault()
     {
@@ -18,7 +27,7 @@ public class MeshGenerator : MonoBehaviour
 
         // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
         /* 시계 방향으로 vertex 찍어야 정방향이다. */
-        mesh.vertices = new Vector3[]
+        mesh.vertices = new []
         {
             /*윗면0*/ new Vector3(0f, 0f, 0f), new Vector3(1f, 0f, 1f), new Vector3(1f, 0f, 0f),
             /*윗면1*/ new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 1f), new Vector3(1f, 0f, 1f),
@@ -35,7 +44,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 삼각형을 이루는 인덱스 설정
-        mesh.triangles = new int[]
+        mesh.triangles = new []
         {
             /*윗면*/
             0, 1, 2,
@@ -58,7 +67,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 각 정점의 노멀 벡터 설정
-        mesh.normals = new Vector3[]
+        mesh.normals = new []
         {
             Vector3.up, Vector3.up, Vector3.up,
             Vector3.up, Vector3.up, Vector3.up,
@@ -75,7 +84,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // UV 좌표 설정
-        mesh.uv = new Vector2[]
+        mesh.uv = new []
         {
             // 윗면
             new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1),
@@ -105,6 +114,182 @@ public class MeshGenerator : MonoBehaviour
         SaveMeshAsset("NavMesh_Cube_0", mesh);
     }
 
+    [MenuItem("Tools/NavTileMesh/Create/NavMesh_SlopePartial_0")]
+    public static void GenerateNavTileMesh_CubeSlope_0()
+    {
+        Mesh mesh = new Mesh();
+
+        // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
+        /* 시계 방향으로 vertex 찍어야 정방향이다. */
+        mesh.vertices = new[]
+        {
+            // 윗면
+            new Vector3(0f, 0f, 0f), new Vector3(1f, 0f, 1f), new Vector3(1f, 0f, 0f),
+            new Vector3(0f, 0f, 0f), new Vector3(0f, 0.5f, 1f), new Vector3(1f, 0f, 1f),
+            //앞면
+            new Vector3(0f, -1f, 0f), new Vector3(1f, 0f, 0f), new Vector3(1f, -1f, 0f),
+            new Vector3(0f, -1f, 0f), new Vector3(0f, 0f, 0f), new Vector3(1f, 0f, 0f),
+            //뒷면
+            new Vector3(1f, 0f, 1f), new Vector3(0f, 0f, 1f), new Vector3(0f, -1f, 1f),
+            new Vector3(1f, 0f, 1f), new Vector3(0f, -1f, 1f), new Vector3(1f, -1f, 1f),
+            //왼쪽
+            new Vector3(0f, 0f, 0f), new Vector3(0f, -1f, 1f), new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 0f), new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 1f),
+            //오른쪽
+            new Vector3(1f, 0f, 0f), new Vector3(1f, 0, 1f), new Vector3(1f, -1f, 1f),
+            new Vector3(1f, 0f, 0f), new Vector3(1f, -1f, 1f), new Vector3(1f, -1f, 0f),
+            
+            //경사면
+            new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 0.5f, 1),
+            new Vector3(0, 0, 1), new Vector3(1, 0, 1), new Vector3(0, 0.5f, 1)
+        };
+
+        // 삼각형을 이루는 인덱스 설정
+        mesh.triangles = new[]
+        {
+            /*윗면*/
+            0, 1, 2,
+            3, 4, 5,
+            /*앞면*/
+            6, 7, 8,
+            9, 10, 11,
+            /*뒷면*/
+            12, 13, 14,
+            15, 16, 17,
+            /*왼쪽면*/
+            18, 19, 20,
+            21, 22, 23,
+            /*오른쪽면*/
+            24, 25, 26,
+            27, 28, 29,
+            
+            //왼+경사
+            30,31,32,
+            
+            //오른+경사
+            33,34,35
+        };
+
+        // 각 정점의 노멀 벡터 설정
+        mesh.normals = new[]
+        {
+            Vector3.up, Vector3.up, Vector3.up,
+            Vector3.up, Vector3.up, Vector3.up,
+            Vector3.back, Vector3.back, Vector3.back,
+            Vector3.back, Vector3.back, Vector3.back,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.right, Vector3.right, Vector3.right,
+            Vector3.right, Vector3.right, Vector3.right,
+
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+        };
+
+        // UV 좌표 설정 : unused.
+        mesh.uv = new[]
+        {
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+        };
+
+        SaveMeshAsset("NavMesh_SlopePartial_0", mesh);
+    }
+    
+        [MenuItem("Tools/NavTileMesh/Create/NavMesh_SlopePartial_1")]
+    public static void GenerateNavTileMesh_CubeSlope_1()
+    {
+        Mesh mesh = new Mesh();
+
+        // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
+        /* 시계 방향으로 vertex 찍어야 정방향이다. */
+        mesh.vertices = new[]
+        {
+            // 윗면
+            new Vector3(0f, 0.5f, 0f), new Vector3(0f, 1f, 1f), new Vector3(1f, 1f, 1f),
+            new Vector3(0f, 0.5f, 0f), new Vector3(1f, 1f, 1f), new Vector3(1f, 0f, 0f),
+            //앞면
+            new Vector3(0,0,0), new Vector3(0, 0.5f, 0), new Vector3(1,0,0),
+            //뒷면
+            new Vector3(0, 0, 1), new Vector3(1, 0, 1), new Vector3(0, 1, 1),
+            new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1),
+            //왼쪽
+            new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 1),
+            new Vector3(0, 0, 0), new Vector3(0, 1, 1), new Vector3(0, 0.5f, 0),
+            //오른쪽
+            new Vector3(1,0,0), new Vector3(1,1,1), new Vector3(1,0,1),
+        };
+
+        // 삼각형을 이루는 인덱스 설정
+        mesh.triangles = new[]
+        {
+            /*윗면*/
+            0, 1, 2,
+            3, 4, 5,
+            /*앞면*/
+            6, 7, 8,
+            /*뒷면*/
+            9, 10, 11,
+            12, 13, 14,
+            /*왼쪽면*/
+            15, 16, 17,
+            18, 19, 20,
+            /*오른쪽면*/
+            21, 22, 23,
+        };
+
+        // 각 정점의 노멀 벡터 설정
+        mesh.normals = new[]
+        {
+            Vector3.up, Vector3.up, Vector3.up,
+            Vector3.up, Vector3.up, Vector3.up,
+            Vector3.back, Vector3.back, Vector3.back,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.right, Vector3.right, Vector3.right,
+        };
+
+        // UV 좌표 설정 : unused.
+        mesh.uv = new[]
+        {
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            Vector2.zero, Vector2.zero, Vector2.zero,
+            
+            Vector2.zero, Vector2.zero, Vector2.zero,
+        };
+
+        SaveMeshAsset("NavMesh_SlopePartial_1", mesh);
+    }
+    
+    //// Slope ////
+
     [MenuItem("Tools/NavTileMesh/Create/NavMesh_Slope_0")]
     public static void GenerateNavTileMesh_SlopeDefault()
     {
@@ -112,7 +297,7 @@ public class MeshGenerator : MonoBehaviour
 
         // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
         /* 시계 방향으로 vertex 찍어야 정방향이다. */
-        mesh.vertices = new Vector3[]
+        mesh.vertices = new []
         {
             /*윗면*/
             new Vector3(0, 0, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1),
@@ -130,7 +315,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 삼각형을 이루는 인덱스 설정
-        mesh.triangles = new int[]
+        mesh.triangles = new []
         {
             /*윗면*/
             0, 1, 2,
@@ -145,7 +330,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 각 정점의 노멀 벡터 설정
-        mesh.normals = new Vector3[]
+        mesh.normals = new []
         {
             /*윗면*/
             // 실제 빛반사를 하지 않으므로 탄젠트값 대신에 0,1로 구분하였다.
@@ -184,7 +369,7 @@ public class MeshGenerator : MonoBehaviour
 
         // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
         /* 시계 방향으로 vertex 찍어야 정방향이다. */
-        mesh.vertices = new Vector3[]
+        mesh.vertices = new []
         {
             /*윗면*/
             new Vector3(0, 0, 0), new Vector3(0, 0.5f, 1), new Vector3(1, 0.5f, 1),
@@ -202,7 +387,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 삼각형을 이루는 인덱스 설정
-        mesh.triangles = new int[]
+        mesh.triangles = new []
         {
             /*윗면*/
             0, 1, 2,
@@ -217,7 +402,7 @@ public class MeshGenerator : MonoBehaviour
         };
 
         // 각 정점의 노멀 벡터 설정 : 실제 빛반사를 하지 않으므로 탄젠트값 대신에 0,1로 구분하였다.
-        mesh.normals = new Vector3[]
+        mesh.normals = new []
         {
             /*윗면*/
             Vector3.up, Vector3.up, Vector3.up,
@@ -248,82 +433,89 @@ public class MeshGenerator : MonoBehaviour
         SaveMeshAsset("NavMesh_SlopeHalf_0", mesh);
     }
 
-    // 진행 중
-    // [MenuItem("Tools/NavTileMesh/Create/NavMesh_SlopeHalf_2")]
-    // public static void GenerateNavTileMesh_SlopeHalf_2()
-    // {
-    //     Mesh mesh = new Mesh();
-    //
-    //     // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
-    //     /* 시계 방향으로 vertex 찍어야 정방향이다. */
-    //     mesh.vertices = new Vector3[]
-    //     {
-    //         /*윗면0*/ new Vector3(0, 0.5f, 0), new Vector3(1, 0.5f, 0f), new Vector3(1, 1, 1),
-    //         /*윗면1*/ new Vector3(0, 0.5f, 0), new Vector3(1, 1, 1), new Vector3(0, 1, 1),
-    //         /*앞면4*/ new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0.5f, 0),
-    //         /*앞면5*/ new Vector3(0, 0, 0), new Vector3(1, 0.5f, 0), new Vector3(0, 0.5f, 0),
-    //         /*뒷면6*/
-    //         /*뒷면7*/
-    //         /*왼쪽면8*/
-    //         /*왼쪽면9*/
-    //         /*오른쪽면10*/
-    //         /*오른쪽면11*/
-    //     };
-    //
-    //     // 삼각형을 이루는 인덱스 설정
-    //     mesh.triangles = new int[]
-    //     {
-    //         /*윗면*/
-    //         0, 1, 2,
-    //         3, 4, 5,
-    //         /*앞면*/
-    //         6, 7, 8,
-    //         9, 10, 11,
-    //         /*뒷면*/
-    //         12, 13, 14,
-    //         15, 16, 17,
-    //         /*왼쪽면*/
-    //         18, 19, 20,
-    //         21, 22, 23,
-    //         /*오른쪽면*/
-    //         24, 25, 26,
-    //         27, 28, 29,
-    //     };
-    //
-    //     // 각 정점의 노멀 벡터 설정
-    //     mesh.normals = new Vector3[]
-    //     {
-    //         Vector3.up, Vector3.up, Vector3.up,
-    //         Vector3.up, Vector3.up, Vector3.up,
-    //         Vector3.back, Vector3.back, Vector3.back,
-    //         Vector3.back, Vector3.back, Vector3.back,
-    //         Vector3.forward, Vector3.forward, Vector3.forward,
-    //         Vector3.forward, Vector3.forward, Vector3.forward,
-    //         Vector3.left, Vector3.left, Vector3.left,
-    //         Vector3.left, Vector3.left, Vector3.left,
-    //         Vector3.right, Vector3.right, Vector3.right,
-    //         Vector3.right, Vector3.right, Vector3.right,
-    //     };
-    //
-    //     // UV 좌표 설정
-    //     mesh.uv = new Vector2[]
-    //     {
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //         Vector3.zero, Vector3.zero, Vector2.zero,
-    //     };
-    //
-    //     SaveMeshAsset("NavMesh_SlopeHalf_2", mesh);
-    // }
+    [MenuItem("Tools/NavTileMesh/Create/NavMesh_SlopeHalf_1")]
+    public static void GenerateNavTileMesh_SlopeHalf_2()
+    {
+        Mesh mesh = new Mesh();
+    
+        // 정점 좌표 설정 (원점이 윗면의 한 꼭지점이 되도록 설정)
+        mesh.vertices = new []
+        {
+            // 윗면
+            new Vector3(0, 0.5f, 0), new Vector3(1, 1, 1), new Vector3(1, 0.5f, 0),
+            new Vector3(0, 0.5f, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1),
+            // 앞면
+            new Vector3(0, 0, 0), new Vector3(1, 0.5f, 0), new Vector3(1, 0, 0),
+            new Vector3(0, 0, 0), new Vector3(0, 0.5f, 0), new Vector3(1, 0.5f, 0),
+            // 뒷면
+            new Vector3(0, 0, 1), new Vector3(1, 0, 1), new Vector3(1, 1, 1),
+            new Vector3(0, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1),
+            // 왼쪽면
+            new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 1),
+            new Vector3(0, 0, 0), new Vector3(0, 1, 1), new Vector3(0, 0.5f, 0),
+            // 오른쪽 면
+            new Vector3(1, 0, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1),
+            new Vector3(1, 0, 0), new Vector3(1, 0.5f, 0), new Vector3(1, 1, 1)
+        };
+    
+        // 삼각형을 이루는 인덱스 설정
+        mesh.triangles = new []
+        {
+            /*윗면*/
+            0, 1, 2,
+            3, 4, 5,
+            /*앞면*/
+            6, 7, 8,
+            9, 10, 11,
+            /*뒷면*/
+            12, 13, 14,
+            15, 16, 17,
+            /*왼쪽면*/
+            18, 19, 20,
+            21, 22, 23,
+            /*오른쪽면*/
+            24, 25, 26,
+            27, 28, 29,
+        };
+    
+        // 각 정점의 노멀 벡터 설정
+        mesh.normals = new []
+        {
+            Vector3.up, Vector3.up, Vector3.up,
+            Vector3.up, Vector3.up, Vector3.up,
+            
+            Vector3.back, Vector3.back, Vector3.back,
+            Vector3.back, Vector3.back, Vector3.back,
+            
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            Vector3.forward, Vector3.forward, Vector3.forward,
+            
+            Vector3.left, Vector3.left, Vector3.left,
+            Vector3.left, Vector3.left, Vector3.left,
+            
+            Vector3.right, Vector3.right, Vector3.right,
+            Vector3.right, Vector3.right, Vector3.right,
+        };
+    
+        // UV 좌표 설정
+        mesh.uv = new Vector2[]
+        {
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            Vector3.zero, Vector3.zero, Vector2.zero,
+    
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            
+            Vector3.zero, Vector3.zero, Vector2.zero,
+            Vector3.zero, Vector3.zero, Vector2.zero,
+        };
+    
+        SaveMeshAsset("NavMesh_SlopeHalf_1", mesh);
+    }
 }
