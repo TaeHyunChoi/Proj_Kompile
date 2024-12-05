a1a8c2a18bb539930dd4872d1ed60cc4d0939914
TaeHyunChoi <relax01@naver.com>
2024-12-06 00:54:55 +0900

    [KOMP-13] Nav Tile - 회전을 반영한 Height Point 계산 (진행 중)
    (진행중) GetHeightMatrix( )

--- Assets/MapSampling/NavTileMesh.cs
+++ Assets/MapSampling/NavTileMesh.cs
@@ -24,4 +24,71 @@ public class NavTileMesh : MonoBehaviour
         
         Debug.Log("Initialized: " + Convert.ToString((long)heightFlag,2));
     }
+    
+    
+
+    // 회전 대응
+    public void TestForRotate(int angle)
+    {
+        // pivot
+        //pivot 계산도 함께 처리할까?
+        
+        // heights
+        var matrix = GetHeightMatrix();
+        int[,] rotated = RotateMatrix(matrix, angle);
+        //이걸 받아서 다시 heightFlag에 저장한다.
+    }
+
+    // 확인 요망
+    private int[,] GetHeightMatrix()
+    {
+        int[,] result = new int[5, 5];
+        
+        //여기서부터...
+
+        return result;
+    }
+
+    private int GetHeight(int index)
+    {
+        var flag = heightFlag >> (index * 4);
+        return (int)(flag & 0b1111);
+    }
+    
+    private int[,] RotateMatrix(int[,] matrix, int angle)
+    {
+        var n = matrix.GetLength(0); // 행렬 크기
+        var rotated = new int[n, n];
+
+        angle %= 360;
+        if (0 == angle)
+        {
+            return matrix;
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                switch (angle)
+                {
+                    case 90:
+                        rotated[j, n - 1 - i] = matrix[i, j];
+                        break;
+                    case 180:
+                        rotated[n - 1 - i, n - 1 - j] = matrix[i, j];
+                        break;
+                    case 270:
+                        rotated[n - 1 - j, i] = matrix[i, j];
+                        break;
+                    // default:
+                    //     rotated[i, j] = matrix[i, j];
+                    //     break;
+                        
+                }
+            }
+        }
+
+        return rotated;
+    }
 }
