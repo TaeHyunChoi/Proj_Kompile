c03b221c333da392663e423c0ef89064bb3f9c61
TaeHyunChoi <relax01@naver.com>
2025-08-09 01:24:41 +0900

    [최태현] Custom_Collider 학습용 스크립트 코드

---
+++ Assets/Script/Editor/MapSampling/MapTileSamplingEditor.cs
@@ -0,0 +1,33 @@
+#if UNITY_EDITOR
+namespace  MapSampling
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomEditor(typeof(MapTileSampling))]
+    public class MyScriptEditor : Editor
+    {
+        private MapTileSampling _sampler;
+        
+        private void Awake()
+        {
+            _sampler = target as MapTileSampling;
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            // 기본 인스펙터 표시
+            base.OnInspectorGUI();
+        
+            if (GUILayout.Button("Save"))
+            {
+                _sampler?.Save();
+            }
+            if (GUILayout.Button("Load"))
+            {
+                _sampler?.Load();
+            }
+        }
+    }  
+}
+#endif
