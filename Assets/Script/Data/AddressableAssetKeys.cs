0c4de777f50d568a194261527e560ac11f844881
TaeHyunChoi <relax01@naver.com>
2025-11-03 00:45:05 +0900

    AssetRef: Assets/Rcs 폴더 디렉토리에서 파일명으로 enum 자료형 만들기 (UnitEditor/Tools/Asset Management/Generate Enums From Folders 메뉴에서 실행)

---
+++ Assets/Script/Data/AddressableAssetKeys.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------
+// 이 파일은 에디터 스크립트(EnumGenerator.cs)에 의하여 자동으로 생성됩니다.
+// 수동으로 편집하지 마십시오.
+// --------------------------------------------------
+public enum ASSET_Prefab
+{
+	None = 0,
+	MapGridLayerPrefab = 1,
+	MapGridPrefab = 2,
+	OP_TitleObject = 3,
+	UI_LoadingCurtain = 4,
+	UI_TitleMenuObject = 5,
+	UIBattle = 6,
+	UIBattle_MenuSlot = 7,
+	UnitBase = 8,
+}
